d168f2dd0fa8b333bb604027a8a18bc5da12c450
ACM-Anik <anikmojumder5252@gmail.com>
2023-02-18 05:59:49 +0600

    partial cards added

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,98 @@
-console.log('everything ok');
+// Another Page(blogs):
 document.getElementById('blogs').addEventListener('click', function(){
     window.location.href = 'blog.html';
 })
 
+let serial = 0;
+// Triangle Card:
 document.getElementById('btn-triangle').addEventListener('click', function(){
     const triangleBString = document.getElementById('triangle-b').value;
     const triangleB = parseFloat(triangleBString);
     const triangleHString = document.getElementById('triangle-h').value;
     const triangleH = parseFloat(triangleHString);
 
-    const multiply = triangleB * triangleH;
-    const textAreaString = document.getElementById('text-area').value ;
-    textAreaString.value = multiply;
+    const triangleMultiply = 0.5 * triangleB * triangleH;
     // displayResult(multiply);
+    serial += 1;
+    console.log(triangleMultiply);
+    displayData(triangleMultiply);
 
-})
-document.getElementById('btn-triangle').addEventListener('click', function(){
+});
 
-})
-document.getElementById('btn-triangle').addEventListener('click', function(){
+// Rectangle Card:
+document.getElementById('btn-rectangle').addEventListener('click', function(){
+    serial += 1;
+    const rectangleW = getElementValueById('rectangle-w');
+    const rectangleI = getElementValueById('rectangle-i');
+    
+    rectangleMultiply = rectangleW * rectangleI;
+    displayData(rectangleMultiply);
+});
 
-})
-document.getElementById('btn-triangle').addEventListener('click', function(){
+// Parallelogram Card:
+document.getElementById('btn-parallelogram').addEventListener('click', function(){
+    const parallelogramB = getElementValueById('parallelogram-b');
+    const parallelogramH = getElementValueById('parallelogram-h');
+    
+    serial += 1;
+    parallelogramMultiply = parallelogramB * parallelogramH;
+    displayData(parallelogramMultiply);
+});
 
-})
-document.getElementById('btn-triangle').addEventListener('click', function(){
 
-})
-document.getElementById('btn-triangle').addEventListener('click', function(){
+// Rhombus card:
+document.getElementById('btn-rhombus').addEventListener('click', function(){
+    const rhombusD1 = getElementValueById('rhombus-d1');
+    const rhombusD2 = getElementValueById('rhombus-d2');
+    
+    serial += 1;
+    rhombusMultiply = 0.5 * rhombusD1 * rhombusD2;
+    displayData(rhombusMultiply);
+});
 
-})
+
+// Pentagon card:
+document.getElementById('btn-pentagon').addEventListener('click', function(){
+    const parallelogramB = getElementValueById('parallelogram-b');
+    const parallelogramH = getElementValueById('parallelogram-h');
+    
+    serial += 1;
+    parallelogramMultiply = parallelogramB * parallelogramH;
+    displayData(parallelogramMultiply);
+});
+
+
+// Ellipse card:
+document.getElementById('btn-ellipse').addEventListener('click', function(){
+    const parallelogramB = getElementValueById('parallelogram-b');
+    const parallelogramH = getElementValueById('parallelogram-h');
+    
+    serial += 1;
+    parallelogramMultiply = parallelogramB * parallelogramH;
+    displayData(parallelogramMultiply);
+});
 
 
 // Common Function:
+function getElementValueById(elementId){
+    const elementString = document.getElementById(elementId).value;
+    const elementValue = parseFloat(elementString);
+    return elementValue;
+}
+
+
+// To create a dynamic table to display data:
+function displayData(triangleMultiply){
+    const dataContainer = document.getElementById('data-area');
+    const tr = document.createElement('tr');
+    tr.innerHTML=`
+        <th class="py-0 my-2">${serial}</th>
+        <td class="py-0 my-2">${2}</td>
+        <td class="py-0 my-2">${triangleMultiply}</td>
+        <button class="text-white bg-sky-500 hover:bg-sky-600 px-4 py-1 font-semibold rounded border border-0 my-2">Covert to m<sup>2</sup></button>
+    `;
+    dataContainer.appendChild(tr);
+}
 
 // function displayResult(result){
 //     const textAreaString = document.getElementById('text-area').value ;
