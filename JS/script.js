843bb97cb970310f5d4813830d4032388442996c
ACM-Anik <anikmojumder5252@gmail.com>
2023-02-18 01:13:04 +0600

    id added

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,20 @@
 console.log('everything ok');
+
+document.getElementById('btn-triangle').addEventListener('click', function(){
+
+})
+document.getElementById('btn-triangle').addEventListener('click', function(){
+
+})
+document.getElementById('btn-triangle').addEventListener('click', function(){
+
+})
+document.getElementById('btn-triangle').addEventListener('click', function(){
+
+})
+document.getElementById('btn-triangle').addEventListener('click', function(){
+
+})
+document.getElementById('btn-triangle').addEventListener('click', function(){
+
+})
